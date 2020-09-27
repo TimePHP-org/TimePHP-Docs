d696e4d8eef9b85468788967725a3c4b6fefa50e
MrAnyx <1ptitigre@gmail.com>
2020-09-27 17:50:38 +0200

    [fix] Structure of changelog.js to make easier to maintain

--- changelog.js
+++ changelog.js
@@ -1,24 +1,23 @@
-
-
 const axios = require("axios")
 require('dotenv').config()
-const fs = require("fs")
+const fs = require("fs");
+const { exit } = require("process");
 
 let content = ""
 
-axios({
-   url: 'https://api.github.com/graphql',
-   method: 'post',
-   headers: {
-      'Authorization': `bearer ${process.env.GITHUB_TOKEN}`,
-      'Content-Type': 'application/json'
-   },
-   data: {
-      query: `
-      query {
-         repository(owner: "TimePHP-Org", name: "TimePHP") {
-            releases(first: 100, orderBy: {field: NAME, direction: ASC}) {
-               nodes {
+const tagNameArg = process.argv.slice(2)[0];
+
+const getReleaseInfo = (tagName) => {
+   return axios.post('https://api.github.com/graphql', {
+      headers: {
+         'Authorization': `bearer ${process.env.GITHUB_TOKEN}`,
+         'Content-Type': 'application/json'
+      },
+      data: {
+         query: `
+         query {
+            repository(owner: "TimePHP-Org", name: "TimePHP") {
+               release(tagName:"${tagName}") {
                   createdAt
                   id
                   isPrerelease
@@ -27,81 +26,88 @@ axios({
                }
             }
          }
+         `
       }
-      `
-   }
-}).then((result) => {
-   let releases = result.data.data.repository.releases.nodes
-   releases.forEach(release => {
-      axios({
-         url: 'https://api.github.com/graphql',
-         method: 'post',
-         headers: {
-            'Authorization': `bearer ${process.env.GITHUB_TOKEN}`,
-            'Content-Type': 'application/json'
-         },
-         data: {
-            query: `
-            query {
-               repository(owner: "TimePHP-Org", name: "TimePHP") {
-                  ref(qualifiedName: "${release.tagName}") {
-                     target {
-                        ... on Commit {
-                           history(first: 100) {
-                              nodes {
-                                 oid
-                                 messageHeadline
-                                 url
-                              }
+   })
+}
+
+const getCommitFromRelease = (tagName) => {
+   return axios.post('https://api.github.com/graphql', {
+      headers: {
+         'Authorization': `bearer ${process.env.GITHUB_TOKEN}`,
+         'Content-Type': 'application/json'
+      },
+      data: {
+         query: `
+         query {
+            repository(owner: "TimePHP-Org", name: "TimePHP") {
+               ref(qualifiedName: "${tagName}") {
+                  target {
+                     ... on Commit {
+                        history(first: 100) {
+                           nodes {
+                              oid
+                              messageHeadline
+                              url
                            }
                         }
                      }
                   }
                }
             }
-            `
          }
-      }).then((result) => {
-         let date = new Date(release.createdAt)
-         let feat = ""
-         let bug = ""
-         content += `
+         `
+      }
+   })
+}
+
+axios.all([
+   getReleaseInfo(tagNameArg),
+   getCommitFromRelease(tagNameArg)
+]).then(results => {
+   const releaseInfo = results[0].data.data.repository.release;
+   const commits = results[1].data.data.repository.ref.target.history.nodes;
+
+   let date = new Date(releaseInfo.createdAt)
+   let feat = ""
+   let bug = ""
+
+   content += `
 ## ${release.tagName}
 
 **Release date** : ${date.toLocaleDateString()} <br>
-**Status** : ${release.isPrerelease ? "Pre-release" : "Release"} <br>
-**link** : [${release.tagName}](${release.url})
+**Status** : ${releaseInfo.isPrerelease ? "Pre-release" : "Release"} <br>
+**link** : [${releaseInfo.tagName}](${releaseInfo.url})
 `
-         let commits = result.data.data.repository.ref.target.history.nodes
-
-         commits.forEach(commit => {
-            if(commit.messageHeadline.startsWith("[add]") || commit.messageHeadline.startsWith("[feat]")){
-               feat += `
+   commits.forEach(commit => {
+      if (commit.messageHeadline.startsWith("[add]") || commit.messageHeadline.startsWith("[feat]")) {
+         feat += `
 - ${commit.messageHeadline.replace("[add]", "").replace("[feat]", "")} ([${commit.oid.substring(0, 7)}](${commit.url}))`
-            } else if(commit.messageHeadline.startsWith("[fix]") || commit.messageHeadline.startsWith("[bug]")){
-               bug += `
+      } else if (commit.messageHeadline.startsWith("[fix]") || commit.messageHeadline.startsWith("[bug]")) {
+         bug += `
 - ${commit.messageHeadline.replace("[fix]", "").replace("[bug]", "")} ([${commit.oid.substring(0, 7)}](${commit.url}))`
-            }
-         })
+      }
+   })
 
-         if(feat !== ""){
-            content += ` 
+   if (feat !== "") {
+      content += ` 
 ### Features
 ${feat}
-            `
-         }
+`
+   }
 
-         if(bug !== ""){
-            content += ` 
-### Bug fixes
-${bug}
+   if (bug !== "") {
+      content += ` 
+   ### Bug fixes
+   ${bug}
+   
+   <br>
+`
+   }
 
-<br>
-            `
-         }
+   let data = fs.writeFileSync("./docs/changelog.md", content, "utf-8")
+   console.log("Changelog file updated !")
 
-         let data = fs.writeFileSync("./docs/changelog.md", content, "utf-8")
-         console.log("Changelog file updated !")
-      })
-   })
-})
+
+});
+
